1812168b58110b03e23e18c805446f966e193503
hafitzrizki <hafitzrizki@gmail.com>
2022-06-12 03:21:55 +0700

    menyelesaikan tantangan Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null;
+
+const promiseOutput = async (emosi) => {
+  try {
+    //gabung array of object nya
+    const arrEmosi = (await promiseTheaterIXX()).concat(await promiseTheaterVGC());
+    //gunakan filter untuk key hasil, hitung panjang size list menggunakan length
+    return arrEmosi.filter((element)=> element.hasil === emosi).length;
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+
 
 module.exports = {
   promiseOutput,
